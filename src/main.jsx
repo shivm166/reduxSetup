5403ce7f58b6d8caa1e061c4ad9cf358fc8b2c7c
shivam gauswami <shivamgauswami86@gmail.com>
2025-09-05 17:58:20 +0530

    react-router-dom added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
 
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
